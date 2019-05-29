e1c576ef25bbe7843e8e534e7555d6dfc4657067
Vincent Ying <vincent_ying_live@outlook.com>
2019-05-29 12:31:22 +0800

    Add User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :phone, presence: true, length: { is: 11 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not create" do
+    bad_params = { :name => "Hola", :phone => "123", :password => "456"}
+    user = User.new(bad_params)
+    assert_not user.save
+  end
+
+  test "should create user" do
+    good_params = { :name => 'Vincent Ying', :phone => "18516043499",
+      :password => "some_good_password" }
+    user = User.new(good_params)
+    assert user.save
+  end
 end
