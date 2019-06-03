f3182b82eeaffa6875a3150b6b42c97c9af283cc
Vincent Ying <vincent_ying_live@outlook.com>
2019-06-03 14:54:43 +0800

    Add initial balance for user account

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :password, presence: true, length: { minimum: 6 }
   validates :phone, presence: true, length: { is: 11 }
+  after_create :set_initial_balance
 
   def authenticate
     real_user = User.find_by(phone: self.phone)
@@ -10,5 +11,9 @@ class User < ApplicationRecord
     else
       return false
     end
+  end
+
+  def set_initial_balance
+    self.balance = 5.00
   end
 end
