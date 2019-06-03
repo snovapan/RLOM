da86f172e54c3807df776bff8f317d8c1ab9731e
Vincent Ying <vincent_ying_live@outlook.com>
2019-06-03 09:24:35 +0800

    Add password authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :password, presence: true, length: { minimum: 6 }
   validates :phone, presence: true, length: { is: 11 }
+
+  def authenticate
+    real_user = User.find_by(phone: self.phone)
+    if real_user.password == self.password
+      return true
+    else
+      return false
+    end
+  end
 end
