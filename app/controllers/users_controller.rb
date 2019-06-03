c854d66a6ac8af81c6a4df514761c96d43cc1fe6
Vincent Ying <vincent_ying_live@outlook.com>
2019-06-03 14:53:07 +0800

    Add user validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,12 @@ class UsersController < ApplicationController
 
   private
   def set_user
-    @user = User.find(params[:id])
+    if session[:user_id].nil?
+      redirect_to login_path
+    else
+      @user = User.find(session[:user_id])
+    end
+    
   end
 
   def signup_params
