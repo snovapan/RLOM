c687d1746e65d1be7d4543d245ac6e1e9913af82
Vincent Ying <vincent_ying_live@outlook.com>
2019-06-03 14:54:29 +0800

    Add session test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,5 +1,15 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-
+  def test_routes
+    assert_equal login_path, '/session/login'
+    assert_routing(
+      { :path => "/session/login", :method => :get },
+      { :controller => "sessions", :action => "new" }
+    )
+    assert_routing(
+      { :path => "/session/login", :method => :post },
+      { :controller => "sessions", :action => "create" }
+    )
+  end
 end
