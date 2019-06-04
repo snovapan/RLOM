7b125eb5bbb12ff428ef467bdfb3bdad9e12c824
Vincent Ying <vincent_ying_live@outlook.com>
2019-06-04 23:19:26 +0800

    Update Item controller

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,10 +1,18 @@
 class ItemController < ApplicationController
   def show
+    @item = Item.find_by(params[:id])
   end
 
   def index
+    @items = Item.all
   end
 
   def new
+  end
+
+  def create
+  end
+
+  def edit
   end
 end
