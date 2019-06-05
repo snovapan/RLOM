8f02860de1256fa567188d4579933a8ffec3a3e5
Vincent Ying <vincent_ying_live@outlook.com>
2019-06-05 23:10:07 +0800

    Add annotation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  price       :string
+#  stock       :string
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Item < ApplicationRecord
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  password   :string
+#  phone      :string
+#  email      :string
+#  balance    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
   validates :name, presence: true
   validates :password, presence: true, length: { minimum: 6 }
