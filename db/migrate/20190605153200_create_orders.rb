954aa6833f6491c493933beea0e578dc0a51d5c2
Vincent Ying <vincent_ying_live@outlook.com>
2019-06-05 23:32:42 +0800

    Update order model

--- db/migrate/20190605153200_create_orders.rb
+++ db/migrate/20190605153200_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.reference :item
-      t.reference :user
+      t.references :item, foreign_key: true
+      t.references :user, foreign_key: true
       t.string :quantity
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_041922) do
+ActiveRecord::Schema.define(version: 2019_06_05_153200) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2019_06_04_041922) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "user_id"
+    t.string "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
