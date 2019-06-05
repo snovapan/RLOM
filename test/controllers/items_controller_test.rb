e547eca908015cdc52fc33fc04f9ebf69ee46e2e
Vincent Ying <vincent_ying_live@outlook.com>
2019-06-05 23:23:54 +0800

    Fix bug

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
   def test_routes
-    assert_equal items_path, '/items/index'
+    assert_equal items_path, '/item/index'
     assert_equal item_path(id: "id"), '/item/id'
     assert_routing(
       { :path => "/item/index", :method => :get },
