43f04cb63121896618876ea0a7ace2306b52de0a
Vincent Ying <vincent_ying_live@outlook.com>
2019-06-15 22:50:15 +0800

    Add annotations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :integer          not null, primary key
+#  item_id    :integer
+#  user_id    :integer
+#  quantity   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Order < ApplicationRecord
   belongs_to :item
   belongs_to :user
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :integer          not null, primary key
+#  item_id    :integer
+#  user_id    :integer
+#  quantity   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
