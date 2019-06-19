390b7be469f8f9a0c79d5e240025b1ec4f583aa8
Vincent Ying <vincent_ying_live@outlook.com>
2019-06-19 22:29:05 +0800

    Add navbar login status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def logged_in?
+    if session[:user_id] != nil
+      @current_user = User.find_by(id:session[:user_id])
+      return true
+    else
+      return false
+    end
+  end
 end
