d0d21639fdf3b44ef374d926d98c4e49ad9b3fbc
Vincent Ying <vincent_ying_live@outlook.com>
2019-06-22 09:52:54 +0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get    'site/index',     to: 'site#index'
-  get    'session/login',  to: 'sessions#new',    as: 'login'
-  post   'session/login',  to: 'sessions#create', as: nil
-  get    'session/logout', to: 'sessions#destroy',as: 'logout'
-
+  get   'site/index',     to: 'site#index'
+  get   'session/login',  to: 'sessions#new',    as: 'login'
+  post  'session/login',  to: 'sessions#create', as: nil
+  get   'session/logout', to: 'sessions#destroy',as: 'logout'
+  get   'users/cart',     to: 'users#cart',      as: 'users_cart'
   resources :users
   resources :items
   root   'site#index'
