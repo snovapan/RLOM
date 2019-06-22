016269aab2a183735172dfc675e53a7bbb604633
Vincent Ying <vincent_ying_live@outlook.com>
2019-06-23 01:45:29 +0800

    Fix @user rendering bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(phone: login_params[:phone])
-    if @user.nil?
+    @user = User.new(login_params)
+    registered_user = User.find_by(phone: login_params[:phone])
+    if registered_user.nil?
       flash[:error] = "用户不存在"
-      render :new, :user => @user
+      render :new, object: @user
       return
     end
-    if @user.authenticate(login_params[:password])
+    if registered_user.authenticate(login_params[:password])
+      @user = registered_user
       session[:user_id] = @user.id
       redirect_to @user
     else
       flash[:error] = "密码错误"
-      render :new
+      render :new, object: @user
     end
   end
 
