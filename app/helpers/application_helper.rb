6724b356946e323857f4481d018b41c67be95779
Vincent Ying <vincent_ying_live@outlook.com>
2019-06-25 13:06:13 +0800

    Update cart view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
       return false
     end
   end
+
+  def current_user
+    @current_user ||= User.find_by(id:session[:user_id])
+    if @current_user.nil?
+      @current_user = User.new
+    end
+    return @current_user
+  end
 end
