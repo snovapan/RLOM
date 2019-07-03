aabb1283f26a173b4f1407daab3242d5b2a8bbe9
Vincent Ying <vincent_ying_live@outlook.com>
2019-07-03 18:59:51 +0800

    Add controllers shared function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  private
+  def current_user
+    @current_user ||= User.find_by(id:session[:user_id])
+  end
+
+  def set_user
+    if session[:user_id].nil?
+      redirect_to login_path
+    else
+      @user = current_user
+    end
+    
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,20 +34,6 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-  def current_user
-    @current_user ||= User.find_by(id:session[:user_id])
-  end
-
-  def set_user
-    if session[:user_id].nil?
-      redirect_to login_path
-    else
-      @user = current_user
-    end
-    
-  end
-
   def signup_params
     params.require(:user).permit(:name, :password, :phone)
   end
