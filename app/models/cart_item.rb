49f615420167aabf25a70d7605353c46b8b917e5
Vincent Ying <vincent_ying_live@outlook.com>
2019-07-03 19:00:12 +0800

    Add annotation

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: cart_items
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  item_id    :integer
+#  quantity   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class CartItem < ApplicationRecord
   belongs_to :user
   belongs_to :item
--- test/models/cart_item_test.rb
+++ test/models/cart_item_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: cart_items
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  item_id    :integer
+#  quantity   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class CartItemTest < ActiveSupport::TestCase
