231a27d9e8f1e3129d4ce791629c6b1d4081a80c
godape <pan.wj@qq.com>
2019-07-18 20:04:45 +0800

    添加商城首页及其他页面改善

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,31 @@
 class SiteController < ApplicationController
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item
+    else
+      flash[:error] = "添加失败"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :stock, :description, :image)
   end
 end
