7dd14d3dc7af9cf66ffb8b88ad3c5592644939fb
Vincent Ying <vincent_ying_live@outlook.com>
2019-07-19 11:19:52 +0800

    Add amin scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :admins
   get   'site/index',     to: 'site#index'
   get   'session/login',  to: 'sessions#new',    as: 'login'
   post  'session/login',  to: 'sessions#create', as: nil
