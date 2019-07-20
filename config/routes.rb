8a451cc4d24cee7db53441a0996e97ccdaceeebf
Vincent Ying <vincent_ying_live@outlook.com>
2019-07-20 23:25:29 +0800

    reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :admins
   get   'site/index',     to: 'site#index'
   get   'session/login',  to: 'sessions#new',    as: 'login'
   post  'session/login',  to: 'sessions#create', as: nil
   get   'session/logout', to: 'sessions#destroy',as: 'logout'
   get   'users/cart',     to: 'users#cart',      as: 'users_cart'
   get   'carts/show',     to: 'carts#show',      as: 'carts'
+  
+  resources :admins
   resources :users
   resources :orders
   resources :items do
