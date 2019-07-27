b1db4bd6dd6463519600730b0a1b61f8dfb34905
Vincent Ying <vincent_ying_live@outlook.com>
2019-07-27 19:08:08 +0800

    Add balance check

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,7 +14,19 @@ class OrdersController < ApplicationController
     @order.user = current_user
     @order.item = Item.find(new_order_params[:item_id])
     @order.quantity = new_order_params[:quantity]
+    
+    # 检查余额
+    if current_user.balance < @order.item.price
+      flash[:error] = "余额不足"
+      @item = @order.item
+      @quantity = @order.quantity
+      render :new
+      return
+    end
+
     if @order.save
+      current_user.balance =((current_user.balance.to_f - @order.item.price.to_f )*100).round / 100.0
+      current_user.save
       redirect_to orders_path
     else
       flash[:error] = "购买失败"
