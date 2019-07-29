33e32f1c4d8d5c8d382b9ea268645506cc55d252
Vincent Ying <vincent_ying_live@outlook.com>
2019-07-29 19:33:25 +0800

    Change price to be float in Product

---
+++ db/migrate/20190729113007_change_price_to_be_float_in_products.rb
@@ -0,0 +1,5 @@
+class ChangePriceToBeFloatInProducts < ActiveRecord::Migration[5.2]
+  def change
+    change_column :products, :price, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_103251) do
+ActiveRecord::Schema.define(version: 2019_07_29_113007) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 2019_07_29_103251) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "price"
+    t.float "price"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
