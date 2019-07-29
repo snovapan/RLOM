444c3b09c778506a9c23ca4608a55013c52fbf8e
Vincent Ying <vincent_ying_live@outlook.com>
2019-07-29 20:04:29 +0800

    Remove jbuilder

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,22 @@
 class ItemsController < ApplicationController
   before_action :set_user
 
+  # GET /item/1
   def show
     @item = Item.find(params[:id])
   end
 
+  # GET /items
   def index
     @items = Item.all
   end
 
+  # GET /items/new
   def new
     @item = Item.new
   end
 
+  # POST /items
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -23,10 +27,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  # GET /items/1/edit
   def edit
     @item = Item.find(params[:id])
   end
 
+  # PUT/PATCH /items/1
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
@@ -37,6 +43,7 @@ class ItemsController < ApplicationController
     end
   end
 
+  # DELETE /items
   def destroy
     @item = Item.find(params[:id])
     @item.delete
--- app/views/admins/_admin.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! admin, :id, :name, :phone, :password, :created_at, :updated_at
-json.url admin_url(admin, format: :json)
